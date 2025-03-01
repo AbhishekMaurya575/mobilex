1eef00375a6df945ba4eccdfbe2219362445282d
AbhishekMaurya575 <abhishek.maurya257185@gmail.com>
2025-03-01 16:23:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./Pages/Home/Home";
-import Layout from "./components/layout";
+import Layout from "./components/Layout";
 
 
 const router = createBrowserRouter([
